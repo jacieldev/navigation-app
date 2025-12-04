10d36e9fe2bb4d7534ff0894b4a04791d9c10bad
jacieldev <jaciel.dev@gmail.com>
2025-12-04 11:05:50 -0700

    add custom font family

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,27 @@
-import { Slot } from 'expo-router'
-import React from 'react'
-import "./global.css"
+import { useFonts } from 'expo-font';
+import { Slot, SplashScreen } from 'expo-router';
 
+import { useEffect } from 'react';
+import "./global.css";
+
+SplashScreen.preventAutoHideAsync();
 
 const Rootlayout = () => {
+
+   const [fontsLoaded, error] = useFonts({
+      'work-black': require('../assets/fonts/WorkSans-Black.ttf'),
+      'work-light': require('../assets/fonts/WorkSans-Light.ttf'),
+      'work-medium': require('../assets/fonts/WorkSans-Medium.ttf'),
+   })
+
+   useEffect(() => {
+      if (error) throw error;
+
+      if (fontsLoaded) SplashScreen.hideAsync();
+   }, [fontsLoaded, error])
+
+   if (!fontsLoaded && !error) return null;
+
    return <Slot />
 }
 
--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const App = () => {
    return (
-      <View className='mt-10'>
-         <Text className='text-3xl font-bold text-blue-500'>hello world </Text>
-      </View>
+      <SafeAreaView>
+         <View className='mt-6 mx-2.5'>
+            <Text style={{ fontFamily: 'WorkSans-Black' }} className='text-3xl '>
+               hello world
+            </Text>
+
+            <Text className='text-3xl font-work-black'>hello world </Text>
+
+            <Text className='text-3xl font-work-light'>hello world </Text>
+
+            <Text className='text-3xl font-work-medium'>hello world </Text>
+         </View>
+      </SafeAreaView>
    )
 }
 
