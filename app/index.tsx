5656ead06e9388397bb20f32065a59d9bbf08ee2
jacieldev <jaciel.dev@gmail.com>
2025-12-04 10:44:32 -0700

    configuration initial with nativewind

---
+++ app/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App = () => {
+   return (
+      <View className='mt-10'>
+         <Text className='text-3xl font-bold text-blue-500'>hello world </Text>
+      </View>
+   )
+}
+
+export default App;
